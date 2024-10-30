48821968a34815374fdbcb734f3c557cb3f9f587
TheMonist <rogerrosena@gmail.com>
2024-10-29 22:05:18 -0400

    adding dotenv files

--- pool.js
+++ pool.js
@@ -1,3 +1,12 @@
 const { Pool } = require("pg");
+const dotenv = require("dotenv");
 
-module.exports = new Pool({})
+dotenv.config();
+
+module.exports = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+})
