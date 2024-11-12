b17df028cf0d813c4c35ed1adf25799d71df7d2b
TheMonist <rogerrosena@gmail.com>
2024-11-11 21:35:23 -0500

    working on views

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
-
-app.get("/", (req, res) => res.send("Hello World!"));
-
+const path = require("node:path");
+const indexRouter = require("./routes/indexRouter");
+const dotenv = require("dotenv");
 const PORT = process.env.PORT || 3000;
+
+dotenv.config();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
+
+//app.get("/", (req, res) => res.send("Hello World!"));
+
 app.listen(PORT, () => {
     console.log(`TESTING EXPRESS APP ON PORT ${PORT}`);
 })
---
+++ routes/indexRouter.js
@@ -0,0 +1,7 @@
+const { Router } = require("express");
+const bookController = require("./controllers/bookController");
+const indexRouter = Router();
+
+indexRouter.get();
+indexRouter.post();
+indexRouter.delete();
